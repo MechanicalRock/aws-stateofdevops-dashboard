db906869a76e169545143b4ba3792a95f8ab91ad
zainab <maleki68.net@gmail.com>
2021-01-13 15:10:48 +0800

    added alarm creation logic if service-health alarms does not exist plus new tests to cover the usecase

--- src/serviceHealthAlarmGenerator.ts
+++ src/serviceHealthAlarmGenerator.ts
@@ -2,9 +2,10 @@ import { CloudWatch, config } from "aws-sdk";
 
 config.region = "ap-southeast-2";
 export async function handler(event: any): Promise<void> {
+    const appName = "test";
     const cloudwatch = new CloudWatch();
     const params: CloudWatch.DescribeAlarmsInput = {
-        AlarmNames: ["test-service-health"],
+        AlarmNames: [`${appName}-service-health`],
     };
     try {
         const response = await cloudwatch.describeAlarms(params).promise();
@@ -12,8 +13,33 @@ export async function handler(event: any): Promise<void> {
             console.log("The alarm exists.\nResponse was: ", response);
             return;
         }
-        console.log("Did not find the alarm.");
+        console.log(`Did not find any alarm named ${appName}-service-health. \nCreating the alarm...`);
+        await createServiceHealthAlarm(appName, cloudwatch);
     } catch (e) {
         console.log("There was an issue finding the alarm: ", e);
     }
+}
+
+async function createServiceHealthAlarm(appName: string, cloudwatch: CloudWatch): Promise<void> {
+    const params: CloudWatch.PutMetricAlarmInput = {
+        AlarmName: `${appName}-service-health`,
+        ComparisonOperator: "GreaterThanOrEqualToThreshold",
+        EvaluationPeriods: 1,
+        ActionsEnabled: true,
+        AlarmDescription: `an overall service health alarm that aggregates the state of all indevidual alarms for ${appName}`,
+        DatapointsToAlarm: 1,
+        Dimensions: [
+            {
+                Name: "product",
+                Value: appName,
+            },
+        ],
+        MetricName: "product-health-metric",
+        Namespace: "Health-Monitoring",
+        Period: 60,
+        Statistic: "Sum",
+        Threshold: 1,
+        TreatMissingData: "missing",
+    };
+    await cloudwatch.putMetricAlarm(params).promise();
 }
--- test/serviceHealthAlarmGenerator.test.ts
+++ test/serviceHealthAlarmGenerator.test.ts
@@ -2,31 +2,113 @@ import * as AWSMock from "aws-sdk-mock";
 import { handler } from "../src/serviceHealthAlarmGenerator";
 
 let cloudWatchDescribeAlarmSpy;
+let cloudWatchPutAlarmSpy;
 describe("serviceHealthAlarmGenerator", () => {
+    const event = {
+        account: "123456789012",
+        region: "ap-southeast-2",
+        detail: {},
+        "detail-type": "Scheduled Event",
+        source: "aws.events",
+        time: "2019-03-01T01:23:45Z",
+        id: "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
+        resources: ["arn:aws:events:ap-southeast-2:123456789012:rule/my-schedule"],
+    };
     describe("service health alarms exist", () => {
-        const event = {
-            account: "123456789012",
-            region: "ap-southeast-2",
-            detail: {},
-            "detail-type": "Scheduled Event",
-            source: "aws.events",
-            time: "2019-03-01T01:23:45Z",
-            id: "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
-            resources: ["arn:aws:events:ap-southeast-2:123456789012:rule/my-schedule"],
-        };
         beforeEach(() => {
-            cloudWatchDescribeAlarmSpy = jest.fn().mockReturnValue({});
+            cloudWatchDescribeAlarmSpy = jest.fn().mockReturnValue({
+                ResponseMetadata: { RequestId: "4750a231-6aa0-40ec-a917-5b6362e1ae5c" },
+                CompositeAlarms: [],
+                MetricAlarms: [
+                    {
+                        AlarmName: "myapp-service-health",
+                        AlarmArn: "arn:aws:cloudwatch:ap-southeast-2:123456789:alarm:myapp-service-health",
+                        AlarmDescription:
+                            "an overall service health alarm that aggregates the state of all indevidual alarms for myapp",
+                        AlarmConfigurationUpdatedTimestamp: "2021-01-13T06:37:05.495Z",
+                        ActionsEnabled: true,
+                        OKActions: [],
+                        AlarmActions: [],
+                        InsufficientDataActions: [],
+                        StateValue: "INSUFFICIENT_DATA",
+                        StateReason: "Unchecked: Initial alarm creation",
+                        StateUpdatedTimestamp: "2021-01-13T06:37:05.495Z",
+                        MetricName: "product-health-metric",
+                        Namespace: "Health-Monitoring",
+                        Statistic: "Sum",
+                        Dimensions: [Array],
+                        Period: 60,
+                        EvaluationPeriods: 1,
+                        DatapointsToAlarm: 1,
+                        Threshold: 1,
+                        ComparisonOperator: "GreaterThanOrEqualToThreshold",
+                        TreatMissingData: "missing",
+                        Metrics: [],
+                    },
+                ],
+            });
             AWSMock.mock("CloudWatch", "describeAlarms", (params, callback) => {
                 callback(null, cloudWatchDescribeAlarmSpy(params));
+            });
+            cloudWatchPutAlarmSpy = jest.fn().mockReturnValue({});
+            AWSMock.mock("CloudWatch", "putMetricAlarm", (params, callback) => {
+                callback(null, cloudWatchPutAlarmSpy(params));
             });
         });
         afterEach(() => {
             AWSMock.restore();
             cloudWatchDescribeAlarmSpy.mockRestore();
         });
-        it("should log, alarm exists", async () => {
+        it("should not create the alarm since the alarm exists", async () => {
             await handler(event);
-            expect(true).toEqual(true);
+            expect(cloudWatchDescribeAlarmSpy).toHaveBeenCalled();
+            expect(cloudWatchPutAlarmSpy).not.toHaveBeenCalled();
+        });
+    });
+
+    describe("service health alarms does not exist", () => {
+        beforeEach(() => {
+            cloudWatchPutAlarmSpy = jest.fn().mockReturnValue({});
+            AWSMock.mock("CloudWatch", "putMetricAlarm", (params, callback) => {
+                callback(null, cloudWatchPutAlarmSpy(params));
+            });
+        });
+        afterEach(() => {
+            AWSMock.restore();
+            cloudWatchDescribeAlarmSpy.mockRestore();
+        });
+        it("should create the alarm when Metric data is empty in describe alarm ", async () => {
+            cloudWatchDescribeAlarmSpy = jest.fn().mockReturnValue({
+                ResponseMetadata: { RequestId: "4750a231-6aa0-40ec-a917-5b6362e1ae5c" },
+                CompositeAlarms: [],
+                MetricAlarms: [],
+            });
+            AWSMock.mock("CloudWatch", "describeAlarms", (params, callback) => {
+                callback(null, cloudWatchDescribeAlarmSpy(params));
+            });
+            await handler(event);
+            expect(cloudWatchDescribeAlarmSpy).toHaveBeenCalled();
+            expect(cloudWatchPutAlarmSpy).toHaveBeenCalled();
+        });
+
+        it("should create the alarm when response is empty object in describe alarm ", async () => {
+            cloudWatchDescribeAlarmSpy = jest.fn().mockReturnValue({});
+            AWSMock.mock("CloudWatch", "describeAlarms", (params, callback) => {
+                callback(null, cloudWatchDescribeAlarmSpy(params));
+            });
+            await handler(event);
+            expect(cloudWatchDescribeAlarmSpy).toHaveBeenCalled();
+            expect(cloudWatchPutAlarmSpy).toHaveBeenCalled();
+        });
+
+        it("should create the alarm when response is null in describe alarm ", async () => {
+            cloudWatchDescribeAlarmSpy = jest.fn().mockReturnValue(null);
+            AWSMock.mock("CloudWatch", "describeAlarms", (params, callback) => {
+                callback(null, cloudWatchDescribeAlarmSpy(params));
+            });
+            await handler(event);
+            expect(cloudWatchDescribeAlarmSpy).toHaveBeenCalled();
+            expect(cloudWatchPutAlarmSpy).toHaveBeenCalled();
         });
     });
 });
