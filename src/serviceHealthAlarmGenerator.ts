1db8d170a52d03685d490f30dc308a94e43cea93
zainab <maleki68.net@gmail.com>
2021-01-13 13:23:10 +0800

    updated describe alarm sdk call for response evaluation

--- src/serviceHealthAlarmGenerator.ts
+++ src/serviceHealthAlarmGenerator.ts
@@ -1,12 +1,19 @@
-import { CloudWatch } from "aws-sdk";
+import { CloudWatch, config } from "aws-sdk";
 
+config.region = "ap-southeast-2";
 export async function handler(event: any): Promise<void> {
     const cloudwatch = new CloudWatch();
     const params: CloudWatch.DescribeAlarmsInput = {
         AlarmNames: ["test-service-health"],
     };
-    cloudwatch.describeAlarms(params, function (err, data) {
-        if (err) console.log("Did not find the alarm. ", err, err.stack);
-        else console.log("Found the alarm. ", data);
-    });
+    try {
+        const response = await cloudwatch.describeAlarms(params).promise();
+        if (response && response.MetricAlarms && response.MetricAlarms.length > 0) {
+            console.log("The alarm exists.\nResponse was: ", response);
+            return;
+        }
+        console.log("Did not find the alarm.");
+    } catch (e) {
+        console.log("There was an issue finding the alarm: ", e);
+    }
 }
